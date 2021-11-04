124b1a8332b76e0094b70de7b166927ac1325b38
DL <neoxrock@gmail.com>
2021-11-05 10:17:45 +1100

    Request test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [data, setData] = useState(null);
+	const onClick = () => {
+		axios.get('https://jsonplaceholder.typicode.com/todos/1').then((r) => {
+			setData(r.data);
+		});
+	};
+	return (
+		<div>
+			<div>
+				<button onClick={onClick}>Request</button>
+			</div>
+			<div>
+				{data && (
+					<textarea
+						rows={7}
+						value={JSON.stringify(data, null, 2)}
+						readOnly={true}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default App;
