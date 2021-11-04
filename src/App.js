a4216ea4c4de153cac24c6ffb7fbe24841674545
DL <neoxrock@gmail.com>
2021-11-05 10:19:57 +1100

    Async

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import axios from 'axios';
 
 const App = () => {
 	const [data, setData] = useState(null);
-	const onClick = () => {
-		axios.get('https://jsonplaceholder.typicode.com/todos/1').then((r) => {
-			setData(r.data);
-		});
-	};
+	// const onClick = () => {
+	// 	axios.get('https://jsonplaceholder.typicode.com/todos/1').then((r) => {
+	// 		setData(r.data);
+	// 	});
+	// };
+
+  // Async
+  const onClick = async () => {
+    try {
+      const r = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      setData(r.data);
+    } catch (e) {
+      console.log(e)
+      
+    }
+  }
 	return (
 		<div>
 			<div>
