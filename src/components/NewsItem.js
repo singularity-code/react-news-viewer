31d998a07f77df709667323ac82880f65285cc2b
DL <neoxrock@gmail.com>
2021-11-05 11:00:48 +1100

    NewsItem

---
+++ src/components/NewsItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NewsItemBlock = styled.div`
+	display: flex;
+
+	.thumbnail {
+		margin-right: 1rem;
+	}
+	.contents {
+	}
+	& + & {
+		margin-top: 3rem;
+	}
+`;
+
+const NewsItem = ({ article }) => {
+	const { title, description, url, urlToImage } = article;
+	return (
+		<div>
+			<NewsItemBlock>
+				{urlToImage && (
+					<div className="thumbnail">
+						<a href={url} target="_blank" rel="noopener noreferrer">
+							<img src={urlToImage} alt="thumbnail" />
+						</a>
+					</div>
+				)}
+				<div>
+					<h2>
+						<a href={url} target="_blank" rel="noopener noreferrer">
+							{title}
+						</a>
+					</h2>
+					<p>{description}</p>
+				</div>
+			</NewsItemBlock>
+		</div>
+	);
+};
+
+export default NewsItem;
